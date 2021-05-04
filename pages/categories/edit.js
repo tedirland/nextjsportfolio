783975bf015b776119b721b8b1a008ff4bd4d345
Ted Irland <irlandth@gmail.com>
2021-05-04 09:43:33 -0500

    Created subdirectories for portfolios and categories plus shared component directory for Navbar and Hero components

---
+++ pages/categories/edit.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function edit() {
+  return <h1>EDIT CATEGORIES</h1>;
+}
+
+export default edit;
---
+++ pages/portfolios/edit.js
@@ -0,0 +1,3 @@
+const Edit = () => <h1>PORTFOLIO EDIT</h1>;
+
+export default Edit;
