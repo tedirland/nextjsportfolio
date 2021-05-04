b0afb0beeca805a329d9f4904959f6b8f9ad1da2
Ted Irland <irlandth@gmail.com>
2021-05-04 12:42:58 -0500

    Moved Header, Hero, Container Div and div with classname portfolio-app into the _app component. Also wrote some conditional logic to only render the header component when the Component.name is equal to home

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -15,22 +15,4 @@ PortfolioDetail.getInitialProps = ({ query }) => {
   return { query };
 };
 
-// class PortfolioDetail extends React.Component {
-//   //Called on the server
-//   static getInitialProps({ query }) {
-//     //What you return here will get into this.props
-//     console.log(query);
-//     return { query, test: 'hello world', num: 4 + 4 };
-//   }
-//   render() {
-//     const id = this.props.query.id;
-//     console.log(id);
-//     return (
-//       <h1>
-//         Portfolio Details! ID: {id} {this.props.test} {this.props.num}
-//       </h1>
-//     );
-//   }
-// }
-
 export default PortfolioDetail;
