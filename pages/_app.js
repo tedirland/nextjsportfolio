d2e1ea813142376b76bd594fef89b39366030224
Ted Irland <irlandth@gmail.com>
2021-05-04 16:01:30 -0500

    Moved footer into _app component, moved conditional redering logic into a helper function

--- pages/_app.js
+++ pages/_app.js
@@ -4,23 +4,23 @@ import '../styles/index.scss';
 import Navbar from '@/components/shared/Navbar';
 import Hero from '@/components/shared/Hero';
 const MyApp = ({ Component, pageProps }) => {
+  const isHomePage = () => Component.name === 'Home';
   return (
     <div className="portfolio-app">
       <Navbar />
-      {Component.name === 'Home' && <Hero />}
+      {isHomePage() && <Hero />}
       <div className="container">
         <Component {...pageProps} />
       </div>
+      {isHomePage() && (
+        <footer id="sticky-footer" className="py-4 bg-black text-white-50 py-3">
+          <div className="container text-center">
+            <small>Copyright &copy; Your Website</small>
+          </div>
+        </footer>
+      )}
     </div>
   );
 };
 
-MyApp.getInitialProps = async context => {
-  const initialProps =
-    App.getInitialProps && (await App.getInitialProps(context));
-
-  return {
-    pageProps: { appData: 'Hello _App Component', ...initialProps.pageProps },
-  };
-};
 export default MyApp;
