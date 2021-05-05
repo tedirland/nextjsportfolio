7c402d0195490a95683b01463c4ea2973ba94d22
Ted Irland <irlandth@gmail.com>
2021-05-05 15:23:50 -0500

    Set up a create portfolio button to make a request to the apollo server and display newly created cards to the screen

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,18 +1,47 @@
+import { useState } from 'react';
 import axios from 'axios';
 import PortfolioCard from '@/components/portfolios/PortfolioCard';
 import Link from 'next/link';
 
+const graphcreatePortfolio = () => {
+  const query = `
+  mutation CreatePortfolio {
+    createPortfolio(input: {
+      title: "New Job"
+      company: "New Company"
+      companyWebsite: "New Website"
+      location: "New location"
+      jobTitle: "New title"
+      description: "New Desc"
+      startDate: "1/1/2020"
+      endDate: "1/1/2021"
+    }) {
+      _id
+      title
+      description
+      company
+      companyWebsite
+      location
+      startDate
+      endDate
+    }
+  }`;
+  return axios
+    .post('http://localhost:3000/graphql', { query })
+    .then(({ data: graph }) => graph.data)
+    .then(data => data.createPortfolio);
+};
 const fetchPortfolios = () => {
   const query = `
   query Portfolios {
     portfolios {
-      _id,
-    title,
-    description,
-    company,
-    companyWebsite,
-    location,
-    startDate,
+      _id
+    title
+    description
+    company
+    companyWebsite
+    location
+    startDate
     endDate
 
   }
@@ -23,7 +52,13 @@ const fetchPortfolios = () => {
     .then(data => data.portfolios)
     .then(portfolios => portfolios);
 };
-const Portfolios = ({ portfolios }) => {
+const Portfolios = ({ data }) => {
+  const [portfolios, setPortfolios] = useState(data.portfolios);
+  const createPortfolio = async () => {
+    const newPortfolio = await graphcreatePortfolio();
+    const newPortfolios = [...portfolios, newPortfolio];
+    setPortfolios(newPortfolios);
+  };
   return (
     <>
       <section className="section-title">
@@ -32,6 +67,9 @@ const Portfolios = ({ portfolios }) => {
             <h1>Portfolios</h1>
           </div>
         </div>
+        <button className="btn btn-primary" onClick={createPortfolio}>
+          Create Portfolio
+        </button>
       </section>
       <section className="pb-5">
         <div className="row">
@@ -52,7 +90,7 @@ const Portfolios = ({ portfolios }) => {
 
 Portfolios.getInitialProps = async () => {
   const portfolios = await fetchPortfolios();
-  return { portfolios };
+  return { data: { portfolios } };
 };
 
 export default Portfolios;
