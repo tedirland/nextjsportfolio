e56350fedf6600fbd997d22579c3c6f2c1b3475a
Ted Irland <irlandth@gmail.com>
2021-05-07 11:00:07 -0500

    removed graphql logic from the server/index file into a seperate folder in the graphql sub directory. This will make the code more readable and better follow conventions

--- server/index.js
+++ server/index.js
@@ -1,62 +1,16 @@
 const express = require('express');
 const next = require('next');
-const mongoose = require('mongoose');
-
-const { ApolloServer, gql } = require('apollo-server-express');
-
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-//This is built in nextjs request handling
 const handle = app.getRequestHandler();
-
-//resolvers
-const { portfolioQueries, portfolioMutations } = require('./graphql/resolvers');
-//types
-const { portfolioTypes } = require('./graphql/types');
-
-//GraphQL Models
-
-const Portfolio = require('./graphql/models/Portfolio');
 
 require('./db').connect();
 
 app.prepare().then(() => {
   const server = express();
 
-  //Construct a schema, using GRAPHQL schema language
-  const typeDefs = gql`
-    ${portfolioTypes}
-
-    type Query {
-      portfolio(id: ID): Portfolio
-      portfolios: [Portfolio]
-    }
-    type Mutation {
-      createPortfolio(input: PortfolioInput): Portfolio
-      updatePortfolio(id: ID, input: PortfolioInput): Portfolio
-      deletePortfolio(id: ID): ID
-    }
-  `;
-  // The root provides a resolver for each api endpoint
-  const resolvers = {
-    Query: {
-      ...portfolioQueries,
-    },
-    Mutation: {
-      ...portfolioMutations,
-    },
-  };
-
-  const apolloServer = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: () => ({
-      models: {
-        Portfolio: new Portfolio(mongoose.model('Portfolio')),
-      },
-    }),
-  });
+  const apolloServer = require('./graphql').createApolloServer();
   apolloServer.applyMiddleware({ app: server });
 
   server.all('*', (req, res) => {
