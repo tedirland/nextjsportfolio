9265a2cfdeb520b84eb32ca4f357912d7d81190b
Ted Irland <irlandth@gmail.com>
2021-05-11 05:48:52 -0500

    Added register component logic and created a reusable component for the form. Wired up onsubmit event handler to capture date from the form.

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,21 @@
-function register() {
-  return <div>Register</div>;
-}
+import RegisterForm from '../components/forms/RegisterForm';
 
-export default register;
+const Register = () => {
+  const register = registerData => {
+    alert(JSON.stringify(registerData));
+  };
+  return (
+    <>
+      <div className="bwm-form mt-5">
+        <div className="row">
+          <div className="col-md-5 mx-auto">
+            <h1 className="page-title">Register</h1>
+            <RegisterForm onSubmit={register} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Register;
