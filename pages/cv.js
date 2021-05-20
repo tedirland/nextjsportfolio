7fb91ea393da47b6c1bcfb5385d219085d175d2a
Ted Irland <irlandth@gmail.com>
2021-05-20 14:12:59 -0500

    Added CV page and uploaded resume as a PDF

--- pages/cv.js
+++ pages/cv.js
@@ -1,5 +1,15 @@
-function cv() {
-  return <div>CV</div>;
-}
+import BaseLayout from '../layouts/BaseLayout';
+const Cv = () => (
+  <BaseLayout>
+    <div className="row mt-4">
+      <div className="col-md-8 offset-md-2">
+        <iframe
+          src="/Ted.Irland.Resume.pdf"
+          style={{ width: '800px', height: '800px' }}
+        ></iframe>
+      </div>
+    </div>
+  </BaseLayout>
+);
 
-export default cv;
+export default Cv;
